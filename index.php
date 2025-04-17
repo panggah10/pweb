9362897b66bbb44f33391e410b7dae326302b923
Panggah Widiandana <117331216+panggah10@users.noreply.github.com>
2025-04-17 10:23:43 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BELAJAR CRUD PHP</title>
+</head>
+<body>
+    <h2>BELAJAR CRUD MAHASISWA UIM YOGYA</h2>
+    </br>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Jenis Kelamin</th>
+            <th>Aksi</th>
+        </tr>
+        <?php
+            include 'koneksi.php';//mengambil file
+            $no = 1;//variabel penomoran
+            //query untuk menampilkan data
+            $data = mysqli_query($koneksi,"SELECT * from mahasiswa");
+            //memecah array
+            while($d=mysqli_fetch_array($data)){
+        ?>
+        <tr>
+            <td><?php echo $no++;?></td>
+            <td><?php echo $d['nim'];?></td>
+            <td><?php echo $d['nama'];?></td>
+            <td><?php echo $d['alamat'];?></td>
+            <td><?php echo $d['jk'];?></td>
+            <td>EDIT|HAPUS</td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+//koneksi ke database
+$koneksi = mysqli_connect("localhost","root","","akademik");
+//Cek koneksi
+if(mysqli_connect_errno()){
+    echo "Koneksi database gagal : ".mysqli_connect_errno();
+}
+?>
